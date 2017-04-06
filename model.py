47fd3b4a66f74d16e75233935f06b207e283726f
Idris Y <sld7700@gmail.com>
2017-04-06 10:42:02 +0300

    Maybe batch mode is working now - but we need to check, because batch BxB and tf.diag_part is tricky

--- model.py
+++ model.py
@@ -14,21 +14,21 @@ class Model:
 
         A_var = tf.Variable(
             initial_value=tf.truncated_normal(
-                shape=[self._emb_dim, self._vocab_dim],
+                shape=[self._vocab_dim, self._emb_dim],
                 stddev=1 / math.sqrt(self._emb_dim)
             )
         )
         B_var = tf.Variable(
             initial_value=tf.truncated_normal(
-                shape=[self._emb_dim, self._vocab_dim],
+                shape=[self._vocab_dim, self._emb_dim],
                 stddev=1 / math.sqrt(self._emb_dim)
             )
         )
 
-        resp_mult = tf.matmul(B_var, self.response_batch, transpose_b=True)
-        cont_mult = tf.matmul(A_var, self.context_batch)
+        resp_mult = tf.matmul(self.response_batch, B_var)
+        cont_mult = tf.matmul(self.context_batch, A_var)
 
-        self.f = tf.nn.tanh(tf.matmul(tf.transpose(cont_mult), resp_mult))
+        self.f = tf.diag_part(tf.matmul(tf.transpose(cont_mult), resp_mult))
 
         m = 0.01
         self.loss = tf.nn.relu(self.f_neg - self.f + m)
@@ -37,7 +37,7 @@ class Model:
         self.optimizer = tf.train.GradientDescentOptimizer(LR).minimize(self.loss)
 
     def _create_placeholders(self):
-        self.context_batch = tf.placeholder(dtype=tf.float32, name='Context', shape=[None, self._vocab_dim, 1])
-        self.response_batch = tf.placeholder(dtype=tf.float32, name='Response', shape=[None, self._vocab_dim, 1])
+        self.context_batch = tf.placeholder(dtype=tf.float32, name='Context', shape=[None, self._vocab_dim])
+        self.response_batch = tf.placeholder(dtype=tf.float32, name='Response', shape=[None, self._vocab_dim])
         self.f_neg = tf.placeholder(dtype=tf.float32, name='f_neg', shape=[None])
 
